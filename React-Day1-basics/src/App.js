c1d5f6a51e224802f0d4f04cc5329bcf3a41f286
vikrant bhadauria <vikrantbhadauriya2@gmail.com>
2020-02-13 21:28:25 +0530

    Add files via upload

---
+++ React-Day1-basics/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './App.css';
+import { Demo } from './components/Demo'
+
+function App(){
+  return (
+    // <h1>Hello React</h1>
+    <Demo/>
+  );
+}
+
+export default App;
---
+++ React-Day1-basics/src/components/Demo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Demo =()=>{
+    return (
+        <h1>Hello React</h1>
+    );
+}
+
+// export function Demo(){
+//     return(
+//         <h1>Hello React</h1>
+//     );
+// }
+
